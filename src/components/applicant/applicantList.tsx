77920b7187b36ae2fa9aca7b661cd2a08a8c5e60
Giwan Persaud <giwan.persaud@gmail.com>
2022-08-28 14:49:43 +0200

    headers

--- src/components/applicant/applicantList.tsx
+++ src/components/applicant/applicantList.tsx
@@ -1,5 +1,8 @@
 import { ApplicantType } from "../../features/applicant/applicantSlice";
 import ApplicantItem from "./applicantItem";
+import { tableHeaderConstants } from "../../utils/constants";
+
+export const tableHeader = [...Object.values(tableHeaderConstants)];
 
 export type ApplicantListProps = {
     applicantList: ApplicantType[];
@@ -8,6 +11,13 @@ export type ApplicantListProps = {
 const ApplicantList = ({ applicantList }: ApplicantListProps) => {
     return (
         <table>
+            <thead>
+                <tr>
+                    {tableHeader.map((header) => (
+                        <th>{header}</th>
+                    ))}
+                </tr>
+            </thead>
             <tbody>
                 {applicantList.map((applicant, index) => (
                     <ApplicantItem
---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+export const tableHeaderConstants = {
+    firstName: "First name",
+    lastName: "Last name",
+    email: "Email",
+    age: "Age",
+    yeasOfExperience: "Years of experience",
+    positionApplied: "Position applied",
+    applicationDate: "Date of application",
+    status: "status of application",
+}
